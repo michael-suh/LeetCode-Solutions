6172d8a1be1d877f4ee59a30fe3eecd8a7ede249
Michael Suh <michaelsuh9901@gmail.com>
2022-06-27 16:46:22 -0700

    Time: 223 ms (73.44%), Space: 51.1 MB (49.44%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false;
-    }
-    let current = x 
-    let reversed = 0;
-    while (current !== 0) {
-        reversed = reversed * 10 + current % 10;
-        current = Math.floor(current / 10);
-    }
-    return x === reversed;
-        
+   let reversed = x.toString().split('').reverse().join('')
+   return (x.toString() === reversed)
 };
